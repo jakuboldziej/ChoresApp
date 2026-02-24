f4385a1184b0c0deed4cfae2fba38f9ed1271ea6
jakuboldziej <jakub.oldziej@wp.pl>
2026-02-24 08:46:15 +0100

    daily-chores UI

--- app/(tabs)/daily-chores.tsx
+++ app/(tabs)/daily-chores.tsx
@@ -2,7 +2,7 @@ import ChoreModal from '@/components/Chores/ChoreModal'
 import DisplayChores from '@/components/Chores/DisplayChores'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import React, { useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function DailyChoresScreen() {
@@ -10,23 +10,28 @@ export default function DailyChoresScreen() {
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <View className="px-6 pt-10">
-        <Text className="text-4xl font-bold text-gray-800">Dzisiejsze</Text>
-        <Text className="text-gray-500 text-lg">Zresetują się o północy</Text>
-      </View>
+      <ScrollView
+        className="flex-1 px-4"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="px-6 pt-10">
+          <Text className="text-4xl font-bold text-gray-800">Dzisiejsze</Text>
+          <Text className="text-gray-500 text-lg">Zresetują się o północy</Text>
+        </View>
 
-      <View className='w-full flex-1 pt-6'>
-        <DisplayChores
-          currentScreen="chores-daily"
-          pFinished={false}
-        />
+        <View className='w-full flex-1 pt-6'>
+          <DisplayChores
+            currentScreen="chores-daily"
+            pFinished={false}
+          />
 
-        <Text className="font-semibold text-2xl pl-6 mt-4 text-gray-400">Ukończone:</Text>
-        <DisplayChores
-          currentScreen="chores-daily"
-          pFinished={true}
-        />
-      </View>
+          <Text className="font-semibold text-2xl pl-6 mt-4 text-gray-400">Ukończone:</Text>
+          <DisplayChores
+            currentScreen="chores-daily"
+            pFinished={true}
+          />
+        </View>
+      </ScrollView>
 
       <TouchableOpacity
         className="absolute bottom-10 right-10 bg-blue-600 w-16 h-16 rounded-full items-center justify-center shadow-lg"
